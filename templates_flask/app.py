6d167d6414b8a36b253f3c234c3bc8f2bf461231
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2023-04-13 11:00:50 +0000

    parte 2

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -4,7 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="" ,
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
 
-@app.route('/')
-def hello():
-    return render_template('hello.html', name='Valentina')
+@app.route('/units ')
+def unitlist():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult = mycursor.fetchall()
+    return render_template('hclash_units.html', units=myresult)
